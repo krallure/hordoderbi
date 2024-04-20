fd8e2d1077be389e6e2227a31c3115219ee26e41
krallure <cristianorueda018@gmail.com>
2024-04-20 22:42:10 +0200

    banner updated with date and scaleable logo, added non-moving gallery, final touches... waiting for text

--- gallery.js
+++ gallery.js
@@ -1,17 +1,11 @@
-<script>
-var mySwiper = new Swiper('.mySwiper', {
-  // Optional parameters
-  slidesPerView: 2,
-  slidesPerColumn: 2,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-</script>
+function toggleFullscreen(element) {
+    // Check if the element or its parent is fullscreen
+    var isFullscreen = element.classList.contains('fullscreen') || element.parentNode.classList.contains('fullscreen');
+    if (!isFullscreen) {
+        element.classList.add('fullscreen');
+        document.body.style.overflow = 'hidden';
+    } else {
+        element.classList.remove('fullscreen');
+        document.body.style.overflow = '';
+    }
+}
